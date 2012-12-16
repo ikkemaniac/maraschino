b48f10405ec0db53bb664f7cfb47fbcdec26ca8e
ikkemaniac <ikkemaniac@localhost>
2012-12-16 16:52:56 +0100

    Revert "fix GIT error handling"
    This reverts commit eee54b1c03fbc2dbff7870f875ad63bdf5732f59.

--- maraschino/updater.py
+++ maraschino/updater.py
@@ -236,6 +236,7 @@ def gitUpdate():
             logger.log('UPDATER :: Already up to date', 'INFO')
             logger.log('UPDATER :: Git output: ' + str(output), 'DEBUG')
             return 'complete'
+
         elif 'Aborting' in line:
             logger.log('UPDATER :: Unable to update from git: '+line, 'ERROR')
             logger.log('UPDATER :: Output: ' + str(output), 'DEBUG')
